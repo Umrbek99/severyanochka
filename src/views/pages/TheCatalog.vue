97fbf509439aa458c0dc1e7ba5ba2c91e3fcac1c
Umrbek99 <umrbekxudoyberganovpro@gmail.com>
2023-03-02 22:38:04 +0300

    catalog page finished

--- src/views/pages/TheCatalog.vue
+++ src/views/pages/TheCatalog.vue
@@ -1,23 +1,146 @@
 <template>
-  <div class="bg-amber-50">
-    <div class="xl:container xl:max-w-screen-xl">
-      <h1 class="text-[64px] font-bold text-card_p">Каталог</h1>
-      <div class="grid grid-cols-4">
-        <div style="" class="col-span-2">A</div>
-        <div style="" class="col-span-1">b</div>
-        <div style="" class="col-span-1">c</div>
-        <div style="" class="col-span-1">d</div>
-        <div style="" class="col-span-1">e</div>
-        <div style="" class="col-span-1">f</div>
-        <div style="" class="col-span-1">g</div>
-        <div style="" class="col-span-1">h</div>
-        <div style="" class="col-span-1">i</div>
-        <div style="" class="col-span-2">j</div>
-        <div style="" class="col-span-1">k</div>
-        <div style="" class="col-span-2">l</div>
-        <div style="" class="col-span-1">Ulug'bek</div>
+  <div class="bg-amber-50 px-4 py-4">
+    <div class="xl:container xl:max-w-screen-xl pb-4">
+      <h1 class="text-[64px] font-bold text-card_p my-5">Каталог</h1>
+      <div class="grid grid-cols-4 gap-6">
+        <a href="" class="col-span-2">
+          <div class="h-[200px] bg-cover rounded-lg bg-no-repeat catalog_img  flex items-end">
+            <div class="w-full h-[60%] inner_catalog text-white rounded-lg flex items-end justify-start">
+              <p class="pb-3 ml-2 text-[18px] font-bold">Молоко, сыр, яйцо</p>
+            </div>
+          </div>
+        </a>
+        <a href="" class="col-span-1">
+          <div class="h-[200px] bg-cover rounded-lg bg-no-repeat catalog_img1  flex items-end">
+            <div class="w-full h-[50%] inner_catalog text-white rounded-lg flex items-end justify-start">
+              <p class="pb-3 ml-2 text-[18px] font-bold">Хлеб</p>
+            </div>
+          </div>
+        </a>
+        <a href="" class="col-span-1">
+          <div class="h-[200px] bg-cover rounded-lg bg-no-repeat catalog_img2 flex items-end">
+            <div class="w-full h-[50%] inner_catalog text-white rounded-lg flex items-end justify-start">
+              <p class="pb-3 ml-2 text-[18px] font-bold">Фрукты и овощи</p>
+            </div>
+          </div>
+        </a>
+        <a href="" class="col-span-1">
+          <div class="h-[200px] bg-cover rounded-lg bg-no-repeat catalog_img3  flex items-end">
+            <div class="w-full h-[50%] inner_catalog text-white rounded-lg flex items-end justify-start">
+              <p class="pb-3 ml-2 text-[18px] font-bold">Замороженные продукты</p>
+            </div>
+          </div>
+        </a>
+        <a href="" class="col-span-1">
+          <div class="h-[200px] bg-cover rounded-lg bg-no-repeat catalog_img4  flex items-end">
+            <div class="w-full h-[50%] inner_catalog text-white rounded-lg flex items-end justify-start">
+              <p class="pb-3 ml-2 text-[18px] font-bold">Напитки</p>
+            </div>
+          </div>
+        </a>
+        <a href="" class="col-span-1">
+          <div class="h-[200px] bg-cover rounded-lg bg-no-repeat catalog_img5  flex items-end">
+            <div class="w-full h-[50%] inner_catalog text-white rounded-lg flex items-end justify-start">
+              <p class="pb-3 ml-2 text-[18px] font-bold">Кондитерские изделия</p>
+            </div>
+          </div>
+        </a>
+        <a href="" class="col-span-1">
+          <div class="h-[200px] bg-cover rounded-lg bg-no-repeat catalog_img6 col-span-1 flex items-end">
+            <div class="w-full h-[50%] inner_catalog text-white rounded-lg flex items-end justify-start">
+              <p class="pb-3 ml-2 text-[18px] font-bold">Чай, кофе</p>
+            </div>
+          </div>
+        </a>
+        <a href="" class="col-span-1">
+          <div class="h-[200px] bg-cover rounded-lg bg-no-repeat catalog_img7 flex items-end">
+            <div class="w-full h-[50%] inner_catalog text-white rounded-lg flex items-end justify-start">
+              <p class="pb-3 ml-2 text-[18px] font-bold">Бакалея</p>
+            </div>
+          </div>
+        </a>
+        <a href="" class="col-span-1">
+          <div class="h-[200px] bg-cover rounded-lg bg-no-repeat catalog_img8  flex items-end">
+            <div class="w-full h-[50%] inner_catalog text-white rounded-lg flex items-end justify-start">
+              <p class="pb-3 ml-2 text-[18px] font-bold">Здоровое питание</p>
+            </div>
+          </div>
+        </a>
+        <a href="" class="col-span-2">
+          <div class="h-[200px] bg-cover rounded-lg bg-no-repeat catalog_img9  flex items-end">
+            <div class="w-full h-[50%] inner_catalog text-white rounded-lg flex items-end justify-start">
+              <p class="pb-3 ml-2 text-[18px] font-bold">Зоотовары</p>
+            </div>
+          </div>
+        </a>
+        <a href="" class="col-span-1">
+          <div class="h-[200px] bg-cover rounded-lg bg-no-repeat catalog_img10  flex items-end">
+            <div class="w-full h-[50%] inner_catalog text-white rounded-lg flex items-end justify-start">
+              <p class="pb-3 ml-2 text-[18px] font-bold">Детское питание</p>
+            </div>
+          </div>
+        </a>
+        <a href="" class="col-span-2">
+          <div class="h-[200px] bg-cover rounded-lg bg-no-repeat catalog_img11  flex items-end">
+            <div class="w-full h-[50%] inner_catalog text-white rounded-lg flex items-end justify-start">
+              <p class="pb-3 ml-2 text-[18px] font-bold">Мясо, птица, колбаса</p>
+            </div>
+          </div>
+        </a>
+        <a href="" class="col-span-1">
+          <div class="h-[200px] bg-cover rounded-lg bg-no-repeat catalog_img12  flex items-end">
+            <div class="w-full h-[50%] inner_catalog text-white rounded-lg flex items-end justify-start">
+              <p class="pb-3 ml-2 text-[18px] font-bold">Непродовольственные товары</p>
+            </div>
+          </div>
+        </a>
       </div>
     </div>
   </div>
 </template>
-<script></script>
+<script>
+</script>
+<style>
+  .catalog_img {
+    background-image: url(../../assets/Images/Catalogimages/img.png);
+  }
+  .catalog_img1 {
+    background-image: url(../../assets/Images/Catalogimages/img1.png);
+  }
+  .catalog_img2 {
+    background-image: url(../../assets/Images/Catalogimages/img2.png);
+  }
+  .catalog_img3 {
+    background-image: url(../../assets/Images/Catalogimages/img3.png);
+  }
+  .catalog_img4 {
+    background-image: url(../../assets/Images/Catalogimages/img4.png);
+  }
+  .catalog_img5 {
+    background-image: url(../../assets/Images/Catalogimages/img5.png);
+  }
+  .catalog_img6 {
+    background-image: url(../../assets/Images/Catalogimages/img6.png);
+  }
+  .catalog_img7 {
+    background-image: url(../../assets/Images/Catalogimages/img7.png);
+  }
+  .catalog_img8 {
+    background-image: url(../../assets/Images/Catalogimages/img8.png);
+  }
+  .catalog_img9 {
+    background-image: url(../../assets/Images/Catalogimages/img9.png);
+  }
+  .catalog_img10 {
+    background-image: url(../../assets/Images/Catalogimages/img10.png);
+  }
+  .catalog_img11 {
+    background-image: url(../../assets/Images/Catalogimages/img11.png);
+  }
+  .catalog_img12 {
+    background-image: url(../../assets/Images/Catalogimages/img12.png);
+  }
+  .inner_catalog {
+    background: linear-gradient(180deg, rgba(112, 192, 91, 0) 0%, #70C05B 82.81%);
+  }
+</style>
