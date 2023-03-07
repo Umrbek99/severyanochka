47d1e18c4190063a4f9fc03479efe806430b1eaf
Umrbek99 <umrbekxudoyberganovpro@gmail.com>
2023-03-08 00:28:28 +0500

    dist file updated

---
+++ dist/assets/TheMainCatalog-338e3da6.js
@@ -0,0 +1,1 @@
+import{_ as o,r as t,o as r,c as n,e as a}from"./index-4ca00b29.js";const c={data(){return{}}};function s(_,p,i,l,u,d){const e=t("router-view");return r(),n("div",null,[a(e)])}const m=o(c,[["render",s]]);export{m as default};
--- dist/assets/TheMainCatalog-7d8a098b.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,r as t,o as r,c as n,e as a}from"./index-8a463aa5.js";const c={data(){return{}}};function s(_,p,i,l,u,d){const e=t("router-view");return r(),n("div",null,[a(e)])}const m=o(c,[["render",s]]);export{m as default};
